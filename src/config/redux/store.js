3ca6e4efa37c643815e3f9060abd1e36c66b873e
Izaaz Waskito <116268166+izaazwaskito@users.noreply.github.com>
2023-07-13 13:05:49 +0700

    add redux

---
+++ src/config/redux/store.js
@@ -0,0 +1,7 @@
+import { applyMiddleware } from "redux";
+import logger from "redux-logger";
+import thunk from "redux-thunk";
+import rootReducer from "./reducer/rootReducer";
+
+const store = createStore(rootReducer, applyMiddleware(thunk, logger));
+export default store;
